1958fa3e1fe1f18796166722271e3a2fb50d9a67
baseleader <vashukstr8@mail.ru>
2021-12-21 09:43:34 +0300

    add task-1 for lesson-1 v.0.2

--- Lesson-1/task_5.py
+++ Lesson-1/task_5.py
@@ -11,23 +11,13 @@
 import subprocess
 import chardet
 
-args1 = ['ping', 'yandex.ru']
-args2 = ['ping', 'youtube.com']
-subproc_ping1 = subprocess.Popen(args1, stdout=subprocess.PIPE)
-subproc_ping2 = subprocess.Popen(args2, stdout=subprocess.PIPE)
+addresses = [['ping', 'yandex.ru'], ['ping', 'youtube.com']]
+for log in addresses:
 
-for line in subproc_ping1.stdout:
-    result = chardet.detect(line)
-    print(result)
-    line = line.decode(result['encoding']).encode('utf-8')
-    print(line.decode('utf-8'), end='')
-    print('*' * 100)
+    process = subprocess.Popen(log, stdout=subprocess.PIPE)
 
-print('/' * 100)
-
-for line in subproc_ping2.stdout:
-    result = chardet.detect(line)
-    print(result)
-    line = line.decode(result['encoding']).encode('utf-8')
-    print(line.decode('utf-8'), end='')
-    print('*' * 100)
+    for line in process.stdout:
+        result = chardet.detect(line)
+        print(result)
+        line = line.decode(result['encoding']).encode('utf-8')
+        print(line.decode('utf-8'))
--- Lesson-1/task_6.py
+++ Lesson-1/task_6.py
@@ -23,15 +23,20 @@
 
 НАРУШЕНИЕ обозначенных условий - задание не выполнено!!!
 """
-from chardet import detect
 
-some_words = ('сетевое программирование', 'сокет', 'декоратор')
-with open('test_file.txt', 'r+', encoding='utf-8') as some_file:
-    some_file.writelines("%s\n" % line for line in some_words)
-with open('test_file.txt', 'rb') as file:
-    some_file = file.read()
-    encoding = detect(some_file)['encoding']
-    print(encoding)
-with open('test_file.txt', encoding='utf-8') as some_text:
-    for line in some_text:
-        print(line, end="")
+import locale
+
+some_text = ['сетевое программирование', 'сокет', 'декоратор']
+
+with open('test_file.txt', 'w+') as f_n:
+    for i in some_text:
+        f_n.write(i + '\n')
+    f_n.seek(0)
+print(f_n)
+
+file_coding = locale.getpreferredencoding()
+
+with open('test_file.txt', 'r', encoding=file_coding) as f_n:
+    for i in f_n:
+        print(i)
+    f_n.seek(0)
